cf0526f727afe6e173e161bc7caaee3aa931c4f7
Luis Villegas <luisjva16@gmail.com>
2025-03-17 14:35:15 -0400

    wip

--- src/components/ComplaintForm/ComplaintForm.tsx
+++ src/components/ComplaintForm/ComplaintForm.tsx
@@ -2,51 +2,120 @@
 
 import { colors } from "@/config/colors";
 import "@/styles/complaint-form.css";
-import { Grid, Select, Textarea, TextInput } from "@mantine/core";
-import Link from "next/link";
+import { Button, Grid, Select, Textarea, TextInput } from "@mantine/core";
+import { useFormik } from "formik";
+import { useState } from "react";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object({
+	denunciante: Yup.string().required("Este campo es obligatorio"),
+	nombreDenunciante: Yup.string().required("Este campo es obligatorio"),
+	nombreVictima: Yup.string().required("Este campo es obligatorio"),
+	email: Yup.string()
+		.email("Correo electrónico inválido")
+		.required("Este campo es obligatorio"),
+	descripcion: Yup.string().required("Este campo es obligatorio"),
+});
 
 export const ComplaintForm = () => {
+	const [isLoading, setIsLoading] = useState(false);
+	const formik = useFormik({
+		initialValues: {
+			denunciante: "",
+			nombreDenunciante: "",
+			nombreVictima: "",
+			email: "",
+			descripcion: "",
+		},
+		validationSchema,
+		onSubmit: (values) => {
+			setIsLoading(true);
+			setTimeout(() => {
+				setIsLoading(false);
+				formik.resetForm();
+				// alert("Denuncia enviada");
+			}, 1000);
+		},
+	});
+
 	return (
-		<div className="complaint-form" style={{ background: colors.primary }}>
+		<div
+			className="complaint-form"
+			style={{ background: colors.primary }}
+			id="Denuncia"
+		>
 			<div className="container">
 				<div className="complaint-form__container">
 					<h2>¡Denuncia ahora!</h2>
-					<Grid w="100%" mt={32}>
-						<Grid.Col span={6}>
-							<Select
-								placeholder="¿Quién denuncia?"
-								data={[
-									"Víctima",
-									"Familiar de la víctima",
-									"Organización no gubernamental",
-									"Otro",
-								]}
-								// rightSection={icon}
-							/>
-						</Grid.Col>
-						<Grid.Col span={6}>
-							<TextInput placeholder="Nombre y apellido del denunciante" />
-						</Grid.Col>
-						<Grid.Col span={6}>
-							<TextInput placeholder="Nombre de la(s) víctima (s)" />
-						</Grid.Col>
-						<Grid.Col span={6}>
-							<TextInput placeholder="Correo electrónico de contacto" />
-						</Grid.Col>
-						<Grid.Col span={12}>
-							<Textarea
-								placeholder="Describe el incidente"
-								autosize
-								minRows={7}
-								maxRows={7}
-							/>
-						</Grid.Col>
-						<Grid.Col span={12}>
-							<Link href="" className="complaint-form__button">
-								ENVIAR DENUNCIA
-							</Link>
-						</Grid.Col>
-					</Grid>
+					<form onSubmit={formik.handleSubmit}>
+						<Grid style={{ width: "100%" }} mt={32}>
+							<Grid.Col span={6}>
+								<Select
+									placeholder="¿Quién denuncia?"
+									data={[
+										"Víctima",
+										"Familiar de la víctima",
+										"Organización no gubernamental",
+										"Otro",
+									]}
+									{...formik.getFieldProps("denunciante")}
+									error={
+										formik.touched.denunciante && formik.errors.denunciante
+									}
+									onChange={(value) => {
+										formik.setFieldValue("denunciante", value);
+									}}
+								/>
+							</Grid.Col>
+							<Grid.Col span={6}>
+								<TextInput
+									placeholder="Nombre y apellido del denunciante"
+									{...formik.getFieldProps("nombreDenunciante")}
+									error={
+										formik.touched.nombreDenunciante &&
+										formik.errors.nombreDenunciante
+									}
+								/>
+							</Grid.Col>
+							<Grid.Col span={6}>
+								<TextInput
+									placeholder="Nombre de la(s) víctima (s)"
+									{...formik.getFieldProps("nombreVictima")}
+									error={
+										formik.touched.nombreVictima && formik.errors.nombreVictima
+									}
+								/>
+							</Grid.Col>
+							<Grid.Col span={6}>
+								<TextInput
+									placeholder="Correo electrónico de contacto"
+									{...formik.getFieldProps("email")}
+									error={formik.touched.email && formik.errors.email}
+								/>
+							</Grid.Col>
+							<Grid.Col span={12}>
+								<Textarea
+									placeholder="Describe el incidente"
+									autosize
+									minRows={7}
+									maxRows={7}
+									{...formik.getFieldProps("descripcion")}
+									error={
+										formik.touched.descripcion && formik.errors.descripcion
+									}
+								/>
+							</Grid.Col>
+							<Grid.Col span={12}>
+								<Button
+									type="submit"
+									className="complaint-form__button"
+									disabled={isLoading}
+								>
+									{isLoading ? "ENVIANDO..." : "ENVIAR DENUNCIA"}
+								</Button>
+							</Grid.Col>
+						</Grid>
+					</form>
 				</div>
 			</div>
 			<img
